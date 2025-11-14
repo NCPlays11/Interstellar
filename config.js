adfa56c46f8efffbc4c5c01c3224e7eee514efeb
NCPlays11 <nc11plays@gmail.com>
2025-11-14 15:41:13 -0600

    Protecting passwords

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
+require('dotenv').config();
+
+// const user1 = process.env.MICAH_USERNAME;
+// const pass1 = process.env.MICAH_PASSWORD;
+const adminuser = process.env.ADMINUSERNAME;
+const adminpass = process.env.ADMINPASSWORD;
+
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    admin: "verysecretadminpassword",
-    Isaac2012: "Isaac2012",
-    Ham_sandwich_67: "67sadie#",
-    Ironclad276: "schoolkutty",
-    sn6opy: "bloodygrassonmyknees",
-    sadbartsimpson: "5lanehighway",
-    PorkChop13: "Ducpe21^",
-    rs1210: "2233"
+    // user1:pass1,
+    adminuser:adminpass
   },
 };
 
